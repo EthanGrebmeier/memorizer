1e8b6ade444eaa60f7f4d2833e5c1640dbf72ac9
Ethan Grebmeier <ethangrebmeier@gmail.com>
2020-12-21 20:02:18 -0700

    Finalized node server

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static(__dirname + '/public'));
+var path = require('path');
+app.use(express.static('public'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/memorizer.html'));
+});
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() { 
+    console.log('Server running on port ' + port)
 });
